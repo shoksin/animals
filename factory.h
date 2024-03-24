8be3ea13aa4e1b9f2aa03d20052ce6de0b9e527d
shoksin <anaumcik300@gmail.com>
2024-03-24 23:04:25 +0300

    + Factory Pattern

---
+++ factory.h
@@ -0,0 +1,30 @@
+#pragma once
+#include"cat.h"
+#include"dog.h"
+#include"fish.h"
+
+class AnimalFactory{
+public:
+virtual Animal* create(std::istringstream& str) const = 0;
+};
+
+class CatFactory : public AnimalFactory{
+    public:
+    Animal* create(std::istringstream& str)const override{
+        return new Cat(str);
+    }
+};
+
+class DogFactory :public AnimalFactory{
+    public:
+    Animal* create(std::istringstream& str)const override{
+        return new Dog(str);
+    }
+};
+
+class FishFactory : public AnimalFactory{
+    public:
+    Fish* create(std::istringstream& str) const override{
+        return new Fish(str);
+    }
+};
